ed8be2bc98f8c9ee35b9f8cc8f76558634b12025
Brian Rivers <brianrivers0015@gmail.com>
2014-03-01 15:46:06 -0600

    Initial commit

---
+++ Register.aspx
@@ -0,0 +1,155 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Register.aspx.cs" Async="true" Inherits="Register" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Coupons, Coupons, Coupons!</title>
+
+    <!-- Bootstrap Links -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- DateJS -->
+    <script src="js/date.js"></script>
+
+    <script type="text/javascript">
+        $(document).ready(function () {
+
+            // DateTime yearsAgo = new DateTime();
+
+            //$("#dayDropDown").prepend("<option selected='selected' value='0'> " + Date.today().addYears(-18).getDay() + " </option>");
+            //$("#monthDropDown").prepend("<option selected='selected' value='0'> " + Date.today().addYears(-18).getMonth() + " </option>");
+            // $("#yearDropDown").prepend("<option selected='selected' value='0'> " + Date.today().addYears(-18).getFullYear() + " </option>");
+
+
+        })
+
+
+    </script>
+
+    <!-- JQuery -->
+
+    <script src="//code.jquery.com/jquery-1.9.1.js"></script>
+    <script src="//code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
+
+</head>
+<body style="background-color:#222">
+
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="Register.aspx">Coupons, Coupons, Coupons!</a>
+            </div>
+            <div class="collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </div>
+            <!--/.nav-collapse -->
+        </div>
+    </div>
+
+    <div class="container" >
+        <div class="navbar">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#"></a></li>
+                <li><a href="#features"></a></li>
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+            </ul>
+        </div>
+
+       
+     
+        <form id="form1" runat="server">
+            <div class="well">
+            <asp:Panel runat="server" ID="messageContainer" >
+
+                <asp:Panel runat="server" ID="documentContainer" >
+                <!-- Link to favorite programming blog.  Displays curret day/month/year-->
+                <div class="well well-sm">
+
+
+                    <h3>Great Programming Site:</h3>
+                    <a href="http://scotch.io/">
+                        <asp:Label ID="currentDate" runat="server"></asp:Label></a> <br /> <br />
+
+                     <asp:ValidationSummary ID="ValidationSummary1" runat="server" HeaderText="Sorry, there are some required fields:" ForeColor="#CC3300" />
+                </div>
+                     <hr />
+                <div id="formContainer" class="well well-lg">
+                    <!--Name Form -->
+                    <asp:Label ID="nameLabel" Text="Name: " runat="server" /><asp:TextBox ID="nameForm" runat="server"></asp:TextBox>
+                    <asp:RegularExpressionValidator ID="nameValidator" ControlToValidate="nameForm" runat="server" ErrorMessage="Name must be between 3 and 50 letters" ValidationExpression="^[a-zA-Z'.\s]{3,50}$"></asp:RegularExpressionValidator>
+                    <asp:RequiredFieldValidator ID="nameRequiredValidator" ControlToValidate="nameForm" runat="server" ErrorMessage="Your name is a required field!" Text="*"></asp:RequiredFieldValidator>
+                    <br />
+                    <br />
+
+                    <!--Email Form -->
+                    <asp:Label Text="Email: " runat="server" /><asp:TextBox ID="emailForm" runat="server"></asp:TextBox>
+                    <asp:RegularExpressionValidator ID="emailValidValidator" runat="server" ErrorMessage="This is an invalid email address" ControlToValidate="emailForm" ValidationExpression="\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*" Text="*"></asp:RegularExpressionValidator>
+                    <asp:RequiredFieldValidator ID="emailRequiredValidator" runat="server" ControlToValidate="emailForm" ErrorMessage="Your email is a required field!">*</asp:RequiredFieldValidator>
+                    <br />
+                    <br />
+
+                    <!-- Birthdate Form -->
+                    <asp:Label Text="Birthday: " runat="server" />
+                    <!-- Day Form -->
+                    <asp:DropDownList ID="dayDropDown" runat="server" Enabled="False" AutoPostBack="true" OnSelectedIndexChanged="dayDropDown_SelectedIndexChanged" EnableViewState="true">
+                        <asp:ListItem Value="-1">Day</asp:ListItem>
+                    </asp:DropDownList>
+                    <asp:Label Text=" / " runat="server" />
+
+                    <asp:RequiredFieldValidator ID="dayRequiredValidator" ControlToValidate="dayDropDown" runat="server" ErrorMessage="You must enter a day of birth!" InitialValue="-1" Text="*"></asp:RequiredFieldValidator>
+
+                    <!-- Month Form -->
+                    <asp:DropDownList ID="monthDropDown" runat="server" AutoPostBack="True" OnSelectedIndexChanged="monthDropDown_SelectedIndexChanged">
+                        <asp:ListItem Value="-1">Month</asp:ListItem>
+                    </asp:DropDownList>
+                    <asp:Label Text=" / " runat="server" />
+                    <asp:RequiredFieldValidator ID="monthRequiredDropDown" ControlToValidate="monthDropDown" runat="server" ErrorMessage="You must enter a month of birth!" InitialValue="-1" Text="*">*</asp:RequiredFieldValidator>
+
+                    <!-- Year Form -->
+                    <asp:DropDownList ID="yearDropDown" runat="server" AutoPostBack="True" OnSelectedIndexChanged="yearDropDown_SelectedIndexChanged" >
+                        <asp:ListItem Value="-1">Year</asp:ListItem>
+                    </asp:DropDownList>
+                    <asp:RequiredFieldValidator ID="yearRequiredDropDown" ControlToValidate="yearDropDown" runat="server" ErrorMessage="You must enter a year of birth!" InitialValue="-1" Text="*"></asp:RequiredFieldValidator>
+                    <br />
+                    <br />
+
+                    <!-- Favorite Restaurants -->
+                    <asp:Label Text="Please Enter Your Favorite Restaurants" runat="server" />
+                    <br />
+                    <br />
+                    <asp:TextBox ID="restaurantForm" Text="One Restaurant to a line" runat="server" TextMode="MultiLine" Height="400px" Width="400px" />
+                    <asp:RequiredFieldValidator ID="restaurantRequiredForm" ControlToValidate="restaurantForm" runat="server" ErrorMessage="You Must enter at least 1 restaurant" InitialValue="One Restaurant to a line">*</asp:RequiredFieldValidator>
+                    <asp:RequiredFieldValidator ID="restaurantRequiredForm2" ControlToValidate="restaurantForm" runat="server" ErrorMessage="You must enter at least 1 restaurant" >*</asp:RequiredFieldValidator>
+                    <br />
+                    <br />
+
+
+                    <!-- Submit Button -->
+                    <asp:Button ID="submitButton" Text="Submit" runat="server" OnClick="submitButtonClick" UseSubmitBehavior="false" />
+                    </asp:Panel>
+                
+            </asp:Panel>
+                </div>
+                </div>
+            
+             
+            <!-- /.container -->
+        </form>
+    </div>
+
+</body>
+</html>
---
+++ Register.aspx.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net.Mail;
+
+public partial class Register : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //Had to add this to allow for form validation.
+        this.UnobtrusiveValidationMode = System.Web.UI.UnobtrusiveValidationMode.None;
+
+       // ScriptManager scriptManager = ScriptManager.GetCurrent(Page);
+        //scriptManager.
+        //Page.Form.Controls.Add(UpdatePanel1);
+
+        
+        currentDate.Text = "Suggested reading for " + DateTime.Now.ToLongDateString();
+
+        //Normally I would prevent the 
+        populateYears();
+        populateMonths();     
+    }
+
+  
+    protected void yearDropDown_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        //Checks to see if both yearDropDown and monthDropDown are away from default value.  If so, enables dayDropdown and populates
+        if (int.Parse(yearDropDown.SelectedValue.ToString()) != -1 && int.Parse(monthDropDown.SelectedValue.ToString()) != -1)
+        {
+                dayDropDown.Enabled = true;
+                populateDays();
+               // UpdatePanel1.Update();
+                
+        }
+        else
+        {
+            dayDropDown.Enabled = false;
+        }
+        
+    }
+
+    protected void monthDropDown_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        //Checks to see if both yearDropDown and monthDropDown are away from default value.  If so, enables dayDropdown and populates
+        if (int.Parse(yearDropDown.SelectedValue.ToString()) != -1 && int.Parse(monthDropDown.SelectedValue.ToString()) != -1)
+        { 
+            dayDropDown.Enabled = true;
+                populateDays();
+               // UpdatePanel1.Update();    
+        }
+        else
+        {
+            dayDropDown.Enabled = false;
+        }
+    }
+   
+   public void populateDays(){
+            
+            
+           List<ListItem> daysList = new List<ListItem>();
+           ListItem item = new ListItem("Days", "-1");
+
+            //Clears current lists
+           daysList.Clear();
+           dayDropDown.Items.Clear();
+           
+           dayDropDown.Items.Add(item);
+
+
+           //Creates array of days and adds it to day dropdown
+           for (int count = 1; count <= System.DateTime.DaysInMonth(int.Parse(yearDropDown.SelectedValue.ToString()), int.Parse(monthDropDown.SelectedValue.ToString())); count++)
+           {
+               daysList.Add(new ListItem(count.ToString(), count.ToString()));
+           }
+           dayDropDown.Items.AddRange(daysList.ToArray());
+   }
+
+
+   public void populateMonths() {
+
+       List<ListItem> monthList = new List<ListItem>();
+       const int MONTHS_IN_YEAR = 12;
+
+
+       //Creates array of months and adds it to month dropdown
+       for (int count = 1; count <= MONTHS_IN_YEAR; count++)
+       {
+           monthList.Add(new ListItem(count.ToString(), count.ToString()));
+       }
+       monthDropDown.Items.AddRange(monthList.ToArray());
+      
+   }
+
+   public void populateYears() {
+
+       List<ListItem> yearList = new List<ListItem>();
+
+       for (int count = DateTime.Now.Year; count >= 1900; count--)
+       {
+           yearList.Add(new ListItem(count.ToString(), count.ToString()));
+       }
+       yearDropDown.Items.AddRange(yearList.ToArray());
+       
+   }
+
+
+   protected void submitButtonClick(object sender, EventArgs e)
+   {
+        const int DAYS_IN_18_YEARS = 18 * 365;
+        const double PRICE_FOR_ONE = 5.00;
+        const double PRICE_FOR_MORE = 5.00;
+        const double MAX_PRICE = 20.00;
+        const double MAX_BEFORE_TWENTY = 6;
+
+       //Gets the current time, and the time entered in the form, then subtracts the two.
+        DateTime currentDate = new DateTime();
+        currentDate = DateTime.Now;
+        DateTime enteredDate = new DateTime(int.Parse(yearDropDown.SelectedValue.ToString()), int.Parse(monthDropDown.SelectedValue.ToString()), int.Parse(dayDropDown.SelectedValue.ToString()));
+        System.TimeSpan diff1 = currentDate - enteredDate;
+
+        if (DAYS_IN_18_YEARS < diff1.TotalDays)
+        {
+            //Takes in restaurants from form and splits them by newLine.  Inserts them into array
+            string[] restaurantList = restaurantForm.Text.ToString().Split(new string [] {Environment.NewLine}, StringSplitOptions.None);
+
+            double couponBookPrice = 0;
+            int restLength = restaurantList.Length;
+
+            //Calculates cost of Coupon Book
+            if (restLength == 1)
+            {
+                couponBookPrice = PRICE_FOR_ONE;
+            }
+            else if (restLength > 1 && restLength < MAX_BEFORE_TWENTY)
+            {
+                couponBookPrice = PRICE_FOR_ONE + PRICE_FOR_MORE * restLength-1;
+            } 
+            else
+            {
+                couponBookPrice = MAX_PRICE;
+            }
+
+            //Displays Success Message and calls email function at the end.
+            documentContainer.Visible = false;
+            Label messageLabel = new Label();
+            messageLabel.Text = "Thank you for registering! <br /> We will be in touch. <br /> Please consider ordering a customized coupon book for just " + couponBookPrice + " dollars! <br />" + sendEmail(restaurantList, enteredDate);           
+            messageContainer.Controls.Add(messageLabel);
+           
+        }
+        else
+        {
+
+            documentContainer.Visible = false;
+            Label messageLabel = new Label();
+        
+            messageLabel.Text= "Oops! You have " + ((int)DAYS_IN_18_YEARS - (int)diff1.TotalDays) + " days until you are 18!";
+
+            //Displays Failure message.  Shows number of days until they turn 18
+            messageContainer.Controls.Add(messageLabel);
+        }
+           
+   }
+
+   public string sendEmail(string [] restaurantList, DateTime enteredDate) {
+       //Outputs array of restaurants into a sorted string
+       string restaurantListPrinted = "";
+       Array.Sort(restaurantList);
+       foreach (string rlP in restaurantList)
+       {
+           restaurantListPrinted += rlP + "<br/>";
+       }
+
+       //Creates the Body message
+       string mailMessage = "<br/>Name: " + nameForm.Text.ToString() + "<br /><br />" +
+                           "Email: " + emailForm.Text.ToString() + "<br /><br />" +
+                           "Date Of Birth: " + enteredDate.ToShortDateString() + "<br /><br />" +
+                           "Favorite Restaurants: <br/>" + restaurantListPrinted + "<br/>";
+
+       //Constructs the email
+       MailMessage message = new MailMessage();
+       message.To.Add("brivers1@my.apsu.edu");
+       message.CC.Add(emailForm.Text.ToString());
+       message.From = new MailAddress("brivers1@my.apsu.edu");
+       message.Subject = "New registration submitted on " + DateTime.Now.ToShortDateString();
+       message.Body = mailMessage;
+       message.IsBodyHtml = true;
+
+       //Attempts to send the message, prints error on failure.
+       try
+       {
+           SmtpClient smtp = new SmtpClient("localhost", 25);
+           smtp.Send(message);
+       }
+       catch (Exception ex) {
+           Console.WriteLine("Exception Caught \n" + ex);
+       }
+       return mailMessage;
+
+   }
+   protected void dayDropDown_SelectedIndexChanged(object sender, EventArgs e)
+   {
+       
+  
+   }
+}
+
+/*
+ 
+ <!-- <asp:ScriptManager ID="ScriptManager1" runat="server">
+            </asp:ScriptManager> -->
+
+
+
+
+
+            <!-- Birthdate Form -->
+            <asp:Label Text="Birthday: " runat="server" />
+            <!-- Start Update Panel -->
+           <!-- <asp:UpdatePanel ID="UpdatePanel1" runat="server" OnDataBinding="Page_Load" ChildrenAsTriggers="False" UpdateMode="Conditional">
+                <ContentTemplate> -->
+            
+                    <!-- Day Form -->
+                    <asp:DropDownList ID="dayDropDown" runat="server" Enabled="False" AutoPostBack="true" OnSelectedIndexChanged="dayDropDown_SelectedIndexChanged" EnableViewState="true" >
+                        <asp:ListItem Value="-1">Day</asp:ListItem>
+                    </asp:DropDownList>
+                    <asp:Label Text=" / " runat="server" />
+
+                    <asp:RequiredFieldValidator ID="dayRequiredValidator" ControlToValidate="dayDropDown" runat="server" ErrorMessage="RequiredFieldValidator" InitialValue="-1"></asp:RequiredFieldValidator>
+            
+              
+           
+                    <!-- Month Form -->
+                    <asp:DropDownList ID="monthDropDown" runat="server" AutoPostBack="True" OnSelectedIndexChanged="monthDropDown_SelectedIndexChanged">
+                        <asp:ListItem Value="-1">Month</asp:ListItem>
+                    </asp:DropDownList>
+                    <asp:Label Text=" / " runat="server" />
+                    <asp:RequiredFieldValidator ID="monthRequiredDropDown" ControlToValidate="monthDropDown" runat="server" ErrorMessage="RequiredFieldValidator" InitialValue="-1"></asp:RequiredFieldValidator>
+
+          
+                    <!-- Year Form -->
+                    <asp:DropDownList ID="yearDropDown" runat="server" AutoPostBack="True" OnSelectedIndexChanged="yearDropDown_SelectedIndexChanged">
+                        <asp:ListItem Value="-1">Year</asp:ListItem>
+                    </asp:DropDownList>
+                    <asp:RequiredFieldValidator ID="yearRequiredDropDown" ControlToValidate="yearDropDown" runat="server" ErrorMessage="RequiredFieldValidator" InitialValue="-1"></asp:RequiredFieldValidator>
+                    <br />
+                    <br />
+
+
+                <!-- </ContentTemplate>
+                <Triggers> 
+                    
+                    <asp:PostBackTrigger ControlID="dayDropDown" />
+                </Triggers>
+            </asp:UpdatePanel> -->
+            <!-- End Birthday -->
+ 
+ */
